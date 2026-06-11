d503505899de192a3b305fd5cac076cf29c3feff
Lazizceek01 <azizovlaziz33@gmail.com>
2026-06-11 12:18:12 +0500

    Fix code

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   preview: {
     host: '0.0.0.0',
     port: 3000,
-    ALLOWED_HOSTS: ['clothes.codes', 'www.clothes.codes'],
+    allowedHosts: ['clothes.codes', 'www.clothes.codes'],
   },
   build: {
     outDir: 'dist',
